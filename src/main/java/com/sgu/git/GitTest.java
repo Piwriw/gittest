10ce5c97316bf11b003da10206342dc020c694f8
Piwriw <Piwriw@huan.com>
2021-04-27 19:48:09 +0800

    first commit

---
+++ src/main/java/com/sgu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.sgu.git;
+
+/**
+ * @Author huan
+ * @Date 2021/4/27 19:45
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
