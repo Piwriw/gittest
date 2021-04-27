af56efe616f1112b5b8ee925456e7155c4e7270b
Piwriw <Piwriw@huan.com>
2021-04-27 20:20:37 +0800

    push commit

--- src/main/java/com/sgu/git/GitTest.java
+++ src/main/java/com/sgu/git/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("hello git4");
         System.out.println("master commit");
         System.out.println("hello-fix test");
+        System.out.println("push test");
     }
 }
